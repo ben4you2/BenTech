da11fbfb85f8566bdcf081d564de0a3c18107cb7
Benito <47949106+ben4you2@users.noreply.github.com>
2025-05-16 16:36:23 -0400

    Create closing.js

---
+++ closing.js
@@ -0,0 +1,5 @@
+<script>
+console.log("Wrapping up all my thing and see what will be doing it they will keep on thinking about me.");
+console.log("There are so many  bad intentions around this place and the house and those need yo be stopping thinkng about the house of tante solimise");
+        
+</script>
